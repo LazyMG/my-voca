3d33834dcbffb8bca45d480da723631100d99135
LazyMG <cbfmark@gmail.com>
2024-04-04 22:10:28 +0900

    4/4

--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Button from "../components/elements/Button";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -41,9 +43,30 @@ const Introduction = styled.div`
   border-radius: 15px;
   width: 100%;
   height: 100%;
+  padding-inline: 90px;
+  padding-block: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Row = styled.div`
+  /* background-color: red; */
+  width: 100%;
+  font-size: 30px;
+  display: flex;
+  justify-content: center;
 `;
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const gotoWordPage = () => {
+    navigate("/word");
+  };
+
   return (
     <Wrapper>
       <Container>
@@ -51,7 +74,12 @@ const Home = () => {
           <Title>단어 시험 생성기</Title>
         </Header>
         <Content>
-          <Introduction></Introduction>
+          <Introduction>
+            <Row>Step 1. 시험 범위 선택</Row>
+            <Row>Step 2. 시험 종류 및 매수 선택</Row>
+            <Row>Step 3. 시험지 확인 및 출력</Row>
+            <Button onClick={gotoWordPage} text="시험지 만들기" />
+          </Introduction>
         </Content>
       </Container>
     </Wrapper>
--- src/router/Upload.jsx
+++ src/router/Upload.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Button from "../components/elements/Button";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -10,8 +11,57 @@ const Wrapper = styled.div`
   border-radius: 15px;
 `;
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 40px;
+  display: grid;
+  grid-template-rows: 1fr 3fr;
+  gap: 15px;
+`;
+
+const Header = styled.div`
+  background-color: red;
+  display: flex;
+`;
+
+const Content = styled.div`
+  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const WordUploadContainer = styled.div`
+  background-color: yellow;
+  width: 100%;
+  display: grid;
+`;
+
+const WordUploadRow = styled.div``;
+
+const WordUploadItem = styled.div``;
+
+const ButtonDiv = styled.div`
+  width: 100%;
+  background-color: green;
+`;
+
 const Upload = () => {
-  return <Wrapper>Upload</Wrapper>;
+  return (
+    <Wrapper>
+      <Container>
+        <Header></Header>
+        <Content>
+          <WordUploadContainer></WordUploadContainer>
+          <ButtonDiv>
+            <Button text="취소" />
+            <Button text="Upload" />
+          </ButtonDiv>
+        </Content>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Upload;
